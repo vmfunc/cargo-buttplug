a5a67025cbb3fd25c15f705225cb89cee2dab91c
vmfunc <mel@ud2.rip>
2024-05-11 18:50:04 -0400

    feat: Add cargo execution and exit code handling
    Co-authored-by: Kenneth <
    kennethnym@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,26 @@ mod vibrator;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
-    vibrator::vibrate().await.unwrap();
+    // get all args 
+    let args: Vec<String> = std::env::args().collect();
+    println!("args: {:?}", args);
+
+    // run cargo with args and interate over the exit code
+    let code = std::process::Command::new("cargo")
+        .args(&args[2..])
+        .status()
+        .expect("failed to execute process") //yeah ik
+        .code()
+        .unwrap();
+
+
+    if code == 0 {
+        println!("you deserve a reward~ 💖");
+        vibrator::vibrate().await.unwrap();
+    } else {
+        println!("do better next time~~");
+    }
+
+    // exit
+    std::process::exit(code);
 }
